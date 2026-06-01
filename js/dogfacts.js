1513a5ba56a25c9f36650428477c8dfc7addcaf8
anniethewebcoder <anniethewebcoder@gmail.com>
2026-06-01 19:32:20 -0400

    more dogs

--- js/dogfacts.js
+++ js/dogfacts.js
@@ -2,33 +2,41 @@ api_url = "https://dogapi.dog/api/v2/facts?limit=5";
 
 let heroRight = document.querySelector(".hero-right");
 let factList = document.createElement("ul");
+heroRight.append(factList);
+let factButton = document.querySelector("#factButton");
 
-fetch(api_url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    data.data.forEach((fact) => {
-      let factElement = document.createElement("li");
-      factElement.textContent = fact.attributes.body;
-      factList.append(factElement);
-    });
-  })
-  .catch((err) => console.error(err));
+factButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-// async function fetchDogFacts() {
-//   try {
-//     let res = await fetch(api_url);
-//     let data = await res.json();
-//     console.log(data);
+  //   fetch(api_url)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       data.data.forEach((fact) => {
+  //         let factElement = document.createElement("li");
+  //         factElement.textContent = fact.attributes.body;
+  //         factList.append(factElement);
+  //         console.log(fact.attributes.body);
+  //       });
+  //     })
+  //     .catch((err) => console.error(err));
 
-//     data.data.forEach((data) => {
-//       let fact = document.createElement("li");
-//       fact.textContent = data.attributes.body;
-//       factList.append(fact);
-//     });
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+  async function fetchDogFacts() {
+    try {
+      let res = await fetch(api_url);
+      let data = await res.json();
+      console.log(data);
 
-// fetchDogFacts();
+      data.data.forEach((fact) => {
+        let factElement = document.createElement("li");
+        factElement.textContent = fact.attributes.body;
+        factList.append(factElement);
+        console.log(fact.attributes.body);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  fetchDogFacts();
+});
